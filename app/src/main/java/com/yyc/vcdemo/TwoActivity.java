f0f0b11f814936092950090cc09b7a4bbedeeb79
yaoyongchao <yaoyongchao3g@163.com>
2019-02-21 12:29:00 +0800

    V1.0分支

---
+++ app/src/main/java/com/yyc/vcdemo/TwoActivity.java
@@ -0,0 +1,16 @@
+package com.yyc.vcdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * V1.0测试。
+ */
+public class TwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+}
